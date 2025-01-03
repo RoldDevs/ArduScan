9960f5b529d332309892ed8144bd283a7f5f15df
Harold Edsel Cabaluna <39079376+RoldDevs@users.noreply.github.com>
2025-01-03 15:15:21 +0800

    Add files via upload

---
+++ sketch_jan3a/sketch_jan3a.ino
@@ -0,0 +1,23 @@
+//MainProgrammer: Harold Edsel F. Cabaluna, Vergel O. Macayan
+//HardWare: Allan Hover Bonifacio
+//CodingMembers: Gabryelle T. Hermosa, Jonas H. Alberca
+
+/**
+ BSCS - 4A [Valid Identification Scanner with Arduino]
+ Project Name: ArduScan
+
+  Libraries Used:
+   - MFRC522 {RFID CARD}
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
